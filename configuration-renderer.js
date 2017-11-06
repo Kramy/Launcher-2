a03bf9a54f19bb6c13f0187a39f99e27c5727e3f
Andriy <andsei91@gmail.com>
2017-11-06 22:08:11 +0100

    routing

--- configuration-renderer.js
+++ configuration-renderer.js
@@ -1,12 +1,15 @@
+require("angular");
+require("angular-route");
+
 var app = angular.module('configuration-module', ['ngRoute']);
 
 app.config(($routeProvider) => {
     $routeProvider.when('/', {
-        templateURL: 'configuration-launcher.html'
+        templateUrl: 'configuration-launcher.html'
     }).when('/configuration-launcher', {
-        templateURL: 'configuration-launcher.html'
+        templateUrl: 'configuration-launcher.html'
     }).when('/configuration-application', {
-        templateURL: 'configuration-application.html'
+        templateUrl: 'configuration-application.html'
     }).otherwise({
         redirectTo: '/'
     })
